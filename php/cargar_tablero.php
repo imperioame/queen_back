5c122f24e00be00914143327d5400abac2421a71
Julián <malditagorda@yahoo.com.ar>
2019-07-24 22:16:28 -0300

    pesta a punto endpoint carga de tableros

--- php/cargar_tablero.php
+++ php/cargar_tablero.php
@@ -6,71 +6,53 @@ require "conexion.php";
 
 //recibo información del usuario y datos a guardar
 $correo_usuario = mysqli_real_escape_string($conexion, $_POST['correo']);
-$datos = mysqli_real_escape_string($conexion, $_POST['datos']);
+$tablero = mysqli_real_escape_string($conexion, $_POST['tablero']);
 
-//disecto los datos recibidos
-$datos = json_decode($datos);
-
-//Empiezo por el array de tableros
-$array_tableros = $data->tableros;
+$tablero = json_decode($tablero);
 
 $response = array();
 
-//Lo recorro
-foreach($array_tableros as $obj_tablero){
-    //Preparo la consulta
-    $consulta = "INSERT INTO `tableros`(`idtableros`, `titulo`, `es_destacado`, `es_oculto`, `fecha_creacion`) 
-    VALUES ('$obj_tablero->id_tablero;','$obj_tablero->titulo','$obj_tablero->es_destacado','$obj_tablero->es_oculto','$obj_tablero->fecha_creacion')";
-
+//Preparo la consulta
+$consulta_tablero = "INSERT INTO `queen_tableros`(`titulo`, `es_destacado`, `es_oculto`, `fecha_creacion`) 
+VALUES ('$tablero->titulo','$tablero->es_destacado','$tablero->es_oculto','$tablero->fecha_creacion')";
 
 
 //Lo intento inyectar
-$exito = mysqli_query($conexion, $consulta_carga_elemento);
+$exito = mysqli_query($conexion, $consulta_tablero);
 if ($exito){
     $response['id_tablero'] = mysqli_insert_id($conexion);
     $code = 200;
     $response['mensaje'] = 'ok';
+
+    //Continuo................
+
+    //Averiguo que id tiene este usuario:
+    $consulta_usuario = "SELECT * FROM `queen_usuarios` WHERE `correo`= '$correo_usuario'";
+    $exito = mysqli_query($conexion, $consulta_usuario);
+    if ($exito){
+        $columnas = mysqli_fetch_assoc( $exito );
+        $id_usuario = $columnas['idusuarios'];
+
+        //Encontré al usuario, ahora tengo que hacer la relación tablero - usuario:
+        $id_tablero = $response['id_tablero'];
+        $consulta_usuario_tablero = "INSERT INTO `queen_usuarios_has_tableros`(`usuarios_idusuarios`, `tableros_idtableros`) VALUES ('$id_usuario','$id_tablero')";
+        
+        //Lo intento inyectar
+        $exito = mysqli_query($conexion, $consulta_usuario_tablero);
+        if (! $exito){
+            $response['mensaje'] = 'No se pudo realizar la carga';
+            $code = 400;
+        };
+    }else{
+        $response['mensaje'] = 'No se pudo realizar la carga';
+        $code = 400;
+    };
 }else{
     $response['mensaje'] = 'No se pudo realizar la carga';
     $code = 400;
-}
-/*
-    try {
-        mysqli_query($conexion, $consulta);
-        $response = '200';
-        $code = 200;
-    }
-    catch (exception $e) {
-        $response = $e->getMessage();
-        $code = 500;
-        echo json_encode($response);
-    };
-*/
 };
 
-
-//Sigo por el array de elementos
-$array_elementos = $data->elementos;
-//Lo recorro
-foreach($array_tableros as $obj_elemento){
-    //Preparo la consulta
-    $consulta = "INSERT INTO `queen_elementos`(`idelementos`, `indice_de_elemento`, ` es_lista`, `contenido`, `fecha_deadline`, `fecha_creacion`, `tableros_idtableros`, `status_idstatus`) 
-    VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])";
-
-    //Lo intento inyectar
-    try {
-        mysqli_query($conexion, $consulta);
-        $response = '200';
-        $code = 200;
-    }
-    catch (exception $e) {
-        $response = $e->getMessage();
-        echo json_encode($response);
-        $code = 500;
-    };
-};
-
-
+echo json_encode($response);
 http_response_code($code);
 mysqli_close($conexion);
 
