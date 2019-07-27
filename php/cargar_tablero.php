de7ed786135e070e1540ad04aedf8df46a81f75e
Julián <malditagorda@yahoo.com.ar>
2019-07-27 19:07:12 -0300

    actualización de tableros

--- php/cargar_tablero.php
+++ php/cargar_tablero.php
@@ -16,18 +16,19 @@ $tablero = mysqli_real_escape_string($conexion, $_POST['tablero']);
 //me aseguro de obtener un correo. sinó no prosigo.
 if($correo_usuario != null and  $correo_usuario != ''){
     $response = array();
+    $id_tablero_a_actualizar = $tablero['id_tablero'];
     $titulo_de_tablero = $tablero['titulo'];
     $es_destacado = $tablero['es_destacado'];
     $es_oculto = $tablero['es_oculto'];
     $fecha_creacion_recibida = $tablero['fecha_creacion'];
 
-    if($es_destacado == 'true'){
+    if($es_destacado == 'true' || $es_destacado == true){
         $es_destacado = 1;
     }else{
         $es_destacado = 0;
     }
 
-    if($es_oculto == 'true'){
+    if($es_oculto == 'true'|| $es_oculto == true){
         $es_oculto = 1;
     }else{
         $es_oculto = 0;
@@ -36,51 +37,72 @@ if($correo_usuario != null and  $correo_usuario != ''){
     $fecha_creacion_convertida = strtotime($fecha_creacion_recibida);
     $fecha_creacion = date('d/m/Y', $fecha_creacion_convertida);
 
-
+    //Debuggeo:
     //$response['post_tablero'] = $_POST['tablero'];
     //$response['post_correo'] = $_POST['correo'];
     //$response['callback'] = 'recibí: titulo: '.$titulo_de_tablero.' destacado: '.$es_destacado.' oculto: '.$es_oculto.' fecha de creación: '.$fecha_creacion;
 
-    //Preparo la consulta
-    $consulta_tablero = "INSERT INTO `queen_tableros`(`titulo`, `es_destacado`, `es_oculto`, `fecha_creacion`) 
-    VALUES ('$titulo_de_tablero','$es_destacado','$es_oculto','$fecha_creacion')";
+    //Me fijo si debo actualizar o crear un tablero nuevo:
+    //Si el id del tablero recibido es '-1' entonces debo crear, sinó, debo actualizar
+    if($id_tablero_a_actualizar == -1){
+        //El tablero no existe, debo crearlo
+        //Preparo la consulta
+        $consulta_tablero = "INSERT INTO `queen_tableros`(`titulo`, `es_destacado`, `es_oculto`, `fecha_creacion`) 
+        VALUES ('$titulo_de_tablero','$es_destacado','$es_oculto','$fecha_creacion')";
 
-    //Lo intento inyectar
-    $exito = mysqli_query($conexion, $consulta_tablero);
-    if ($exito){
-        $response['id_tablero'] = mysqli_insert_id($conexion);
-        $code = 200;
-        $response['mensaje'] = 'ok';
-
-        //Continuo................
-
-        //Averiguo que id tiene este usuario:
-        $consulta_usuario = "SELECT * FROM `queen_usuarios` WHERE `correo`= '$correo_usuario'";
-        $exito = mysqli_query($conexion, $consulta_usuario);
+        //Lo intento inyectar
+        $exito = mysqli_query($conexion, $consulta_tablero);
         if ($exito){
-            $columnas = mysqli_fetch_assoc( $exito );
-            $id_usuario = $columnas['idusuarios'];
+            $response['id_tablero'] = mysqli_insert_id($conexion);
+            $code = 200;
+            $response['mensaje'] = 'Se ha creado un nuevo tablero en la bd correctamente.';
 
-            //Encontré al usuario, ahora tengo que hacer la relación tablero - usuario:
-            $id_tablero = $response['id_tablero'];
-            $consulta_usuario_tablero = "INSERT INTO `queen_usuarios_has_tableros`(`usuarios_idusuarios`, `tableros_idtableros`) VALUES ('$id_usuario','$id_tablero')";
+            //Continuo................
 
-            //Lo intento inyectar
-            $exito = mysqli_query($conexion, $consulta_usuario_tablero);
-            if (! $exito){
-                $response['mensaje'] = 'No se pudo vincular el usuario con el tablero';
+            //Averiguo que id tiene este usuario:
+            $consulta_usuario = "SELECT * FROM `queen_usuarios` WHERE `correo`= '$correo_usuario'";
+            $exito = mysqli_query($conexion, $consulta_usuario);
+            if ($exito){
+                $columnas = mysqli_fetch_assoc( $exito );
+                $id_usuario = $columnas['idusuarios'];
+
+                //Encontré al usuario, ahora tengo que hacer la relación tablero - usuario:
+                $id_tablero = $response['id_tablero'];
+                $consulta_usuario_tablero = "INSERT INTO `queen_usuarios_has_tableros`(`usuarios_idusuarios`, `tableros_idtableros`) VALUES ('$id_usuario','$id_tablero')";
+
+                //Lo intento inyectar
+                $exito = mysqli_query($conexion, $consulta_usuario_tablero);
+                if (! $exito){
+                    $response['mensaje'] = 'No se pudo vincular el usuario con el tablero';
+                    $code = 400;
+                };
+            }else{
+                $response['mensaje'] = 'No se encontró el usuario dueño de este tablero';
                 $code = 400;
             };
         }else{
-            $response['mensaje'] = 'No se encontró el usuario dueño de este tablero';
+            $response['mensaje'] = 'No se pudo insertar el tablero';
             $code = 400;
         };
     }else{
-        $response['mensaje'] = 'No se pudo insertar el tablero';
-        $code = 400;
-    };
+        //El tablero existe - debo actualizar
+        $consulta_tablero = "UPDATE `queen_tableros`
+        SET `titulo`= '$titulo_de_tablero',`es_destacado`= '$es_destacado',`es_oculto`= '$es_oculto',`fecha_creacion`= '$fecha_creacion'
+        WHERE `idtableros` = '$id_tablero_a_actualizar'";
+        
+        $exito = mysqli_query($conexion, $consulta_tablero);
+
+        if($exito){
+            $code = 200;
+            $response['mensaje'] = 'Se ha actualizado el tablero correctamente';
+        }else{
+            $response['mensaje'] = 'No se pudo actualizar el tablero';
+            $code = 400;
+        };
+    }
+
 }else{
-    $response['mensaje'] = 'No se recibió usuario, no se puede insertar el tablero';
+    $response['mensaje'] = 'No se recibió usuario, no se pudo ejecutar la acción';
     $code = 400;
 };
 
