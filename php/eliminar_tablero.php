a4d94ac7df076d19f968f6f57b59f04cde8fbe97
Julián <malditagorda@yahoo.com.ar>
2019-07-29 01:45:54 -0300

    ajustes en endpoint para borrar tableros

--- php/eliminar_tablero.php
+++ php/eliminar_tablero.php
@@ -13,17 +13,28 @@ $id_tablero = mysqli_real_escape_string($conexion, $_POST['id_tablero']);
 //me aseguro de obtener un correo. sinó no prosigo.
 if($correo_usuario != null and  $correo_usuario != ''){
     
+    //Primero borro la relación usuario-tablero
     //Preparo la consulta:
-    $consulta_tablero = "DELETE FROM `queen_tableros` WHERE `idtableros` = $id_tablero";
-
+    $consulta_rel_us_tab = "DELETE FROM `queen_usuarios_has_tableros` WHERE `tableros_idtableros` = $id_tablero";
+    
     try{
-        $exito = mysqli_query($conexion, $consulta_tablero);
+        $exito = mysqli_query($conexion, $consulta_rel_us_tab);
         if($exito){
-            $response['mensaje'] = 'Se ha eliminado el tablero correctamente.';
-            $code = 200;
+            //Continuo con la tabla de tableros
+            //Preparo la consulta:
+            $consulta_tablero = "DELETE FROM `queen_tableros` WHERE `idtableros` = $id_tablero";
+        
+            $exito = mysqli_query($conexion, $consulta_tablero);
+            if($exito){
+                $response['mensaje'] = 'Se ha eliminado el tablero correctamente.';
+                $code = 200;
 
+            }else{
+                $response['mensaje'] = 'No se ha podido eliminar el tablero';
+                $code = 400;
+            }
         }else{
-            $response['mensaje'] = 'No se ha podido eliminar el tablero por error de SQL';
+            $response['mensaje'] = 'No se ha podido desenlazar el tablero del usuario';
             $code = 400;
         }
 
